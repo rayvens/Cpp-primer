85a084c575e3adf76f10a56232d2c353ead12a7e
rayvens <rayvens.gear@gmail.com>
2020-11-26 02:00:13 -0400

    Add new function to main exercise and person exercise

--- person exercise/main.cpp
+++ person exercise/main.cpp
@@ -2,17 +2,23 @@
 
 struct person{
     std::string getname() const { return name; }
-    std::string getaddress() { return address; }
-
-
+    std::string getaddress() const { return address; }
     std::string name;
     std::string address;
 };
 
+std::ostream &print(std::ostream& os, const person& person1){
+    os << person1.getname() << " " << person1.getaddress();
+    return os;
+
+};
+std::istream &read(std::istream& is, person& person1){
+    is >> person1.name >> person1.address;
+    return is;
+};
+
 int main(){
-
-
-
-
-
+    person alm;
+ read(std::cin,alm);
+ print(std::cout,alm);
 }
